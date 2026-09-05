1f651366efda7dea7603c9b9017110bcda43667a
unknown <poolgomez27@gmail.com>
2026-09-05 08:49:57 -0500

    Resolver conflicto y actualizar roles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,9 @@ import { EditarCategoria } from './editar-categoria/editar-categoria';
 import { GestionCategoria } from './gestion-categoria/gestion-categoria';
 import { LoginComponent } from './login/login';
 import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta';
-import { Rol } from './rol/rol';
+import { CrearRoles } from './rol/crear-rol/crear-rol';
+import { ListarRoles } from './rol/listar-rol/listar-rol';
+
 
 export const routes: Routes = [
   {
@@ -36,6 +38,7 @@ export const routes: Routes = [
   },
   {
     path: 'editar-categoria',
+<<<<<<< Updated upstream
     component: EditarCategoria
   },
   {
@@ -43,11 +46,17 @@ export const routes: Routes = [
     component: GestionCategoria
   },
   {
-    path: 'rol',
-    component: Rol
-  },
-  {
     path: '**',
     redirectTo: 'login'
-  }
+  },
+  {
+    path: 'rol',
+    component: ListarRoles
+  },
+  {
+    path: 'rol/crear',
+    component: CrearRoles
+  },
+
 ];
+>>>>>>> Stashed changes
