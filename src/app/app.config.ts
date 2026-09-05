de4269016dc1f3ee7115a68f8e55dd0401442d7d
jhonfredyr <riveracjhonf@gmail.com>
2026-09-05 00:57:48 -0500

    agregue gestion de categorias

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,35 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners
+} from '@angular/core';
+
+import {
+  provideRouter
+} from '@angular/router';
+
+import {
+  provideClientHydration
+} from '@angular/platform-browser';
+
+import {
+  provideHttpClient
+} from '@angular/common/http';
+
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
+
   providers: [
+
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+
+    provideRouter(routes),
+
+    provideClientHydration(),
+
+    provideHttpClient()
+
   ]
+
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'gestion-categoria',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'gestion-categoria',
+    loadComponent: () =>
+      import('./gestion-categoria/gestion-categoria')
+        .then(
+          m => m.GestionCategoria
+        )
+  },
+
+  {
+    path: 'crear-categoria',
+    loadComponent: () =>
+      import('./crear-categoria/crear-categoria')
+        .then(
+          m => m.CrearCategoria
+        )
+  },
+
+  {
+    path: 'editar-categoria',
+    loadComponent: () =>
+      import('./editar-categoria/editar-categoria')
+        .then(
+          m => m.EditarCategoria
+        )
+  }
+
+];
